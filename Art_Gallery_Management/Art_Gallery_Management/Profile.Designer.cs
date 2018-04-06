096eebd909b46df60f58198e0e2279b8bd5797a1
manojkun <r.nanimanoj@gmail.com>
2018-04-06 20:39:34 +0530

    LIKES ADDED

--- Art_Gallery_Management/Art_Gallery_Management/Profile.Designer.cs
+++ Art_Gallery_Management/Art_Gallery_Management/Profile.Designer.cs
@@ -28,65 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(309, 145);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(215, 343);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(36)))), ((int)(((byte)(72)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(353, 449);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(134, 62);
             this.button2.TabIndex = 3;
-            this.button2.Text = "PurChase";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Purchase";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(43, 102);
+            this.comboBox1.Location = new System.Drawing.Point(35, 45);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(168, 21);
             this.comboBox1.TabIndex = 4;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(505, 0);
+            this.label9.Location = new System.Drawing.Point(589, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(33, 31);
             this.label9.TabIndex = 14;
             this.label9.Text = "X";
             this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(35, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(168, 226);
+            this.listBox1.TabIndex = 15;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(36)))), ((int)(((byte)(72)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(348, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Painting Name:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(36)))), ((int)(((byte)(72)))));
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 553);
+            this.panel1.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(262, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 33);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Like";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(276, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 33);
+            this.label1.TabIndex = 20;
+            // 
             // Profile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 458);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(619, 553);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Profile";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Profile";
             this.Load += new System.EventHandler(this.Profile_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,10 +164,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
 
     }
 }
--- Art_Gallery_Management/Art_Gallery_Management/Profile.cs
+++ Art_Gallery_Management/Art_Gallery_Management/Profile.cs
@@ -41,7 +41,7 @@ namespace Art_Gallery_Management
          
            db_connect();
             comm = new OracleCommand();
-            comm.CommandText = "select P_id from Painting";
+            comm.CommandText = "select P_id from Painting where P_id not in ( select P_id from Purchase)";
             comm.CommandType = CommandType.Text;
             ds = new DataSet();
             da = new OracleDataAdapter(comm.CommandText, conn);
@@ -49,6 +49,15 @@ namespace Art_Gallery_Management
             dt = ds.Tables["Painting"];
             comboBox1.DataSource = dt.DefaultView;
             comboBox1.DisplayMember = "P_id";
+            listBox1.DataSource = dt.DefaultView;
+            listBox1.DisplayMember = "P_id";
+
+
+            
+
+
+
+
             conn.Close();
         }
 
@@ -63,11 +72,14 @@ namespace Art_Gallery_Management
             db_connect();
             OracleCommand cum = new OracleCommand();
             cum.Connection = conn;
-            cum.CommandText = "insert into Purchase values('" + id + "','" + pid + "SYSDATE)";
+            cum.CommandText = "insert into Purchase values('" + id + "','" + pid + "' , SYSDATE)";
             cum.CommandType = CommandType.Text;
             cum.ExecuteNonQuery();
-            MessageBox.Show("inserted the values");
+            MessageBox.Show("Purchased SuccessFully");
             conn.Close();
+            Form f = new Profile(id);
+            this.Close();
+            f.Show();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -81,11 +93,11 @@ namespace Art_Gallery_Management
             comm = new OracleCommand();
             //string pid = listBox1.SelectedItem.ToString();
             string pid = comboBox1.GetItemText(comboBox1.SelectedItem);
-            label1.Text = pid;
-           comm.CommandText = "select P_name from Painting where P_id ="+pid;
+            label2.Text = pid;
+            comm.CommandText = "select P_name from Painting where P_id ="+pid;
             comm.CommandType = CommandType.Text;
             
-      /*    OracleParameter p1 = new OracleParameter();
+       /*   OracleParameter p1 = new OracleParameter();
             p1.ParameterName = "para";
             p1.DbType = DbType.Int64;
             label1.Text = comboBox1.SelectedItem.ToString();
@@ -98,8 +110,16 @@ namespace Art_Gallery_Management
             da.Fill(ds, "Painting");
             dt = ds.Tables["Painting"];
             dr = dt.Rows[i];
-            label1.Text = dr["P_name"].ToString();
+            label2.Text = dr["P_name"].ToString();
+            imgchange(label2.Text);
+            likes(pid);
             conn.Close();
+        }
+
+        public void imgchange(String iname)
+        {
+            Image image1 = Image.FromFile("C:\\Users\\Manoj\\Desktop\\"+iname+".jpg");
+            pictureBox1.Image = image1;
         }
 
         private void label9_Click(object sender, EventArgs e)
@@ -107,6 +127,55 @@ namespace Art_Gallery_Management
             Application.Exit();
         }
 
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            db_connect();
+            comm = new OracleCommand();
+            string pid = listBox1.GetItemText(listBox1.SelectedItem);
+            //string pid = comboBox1.GetItemText(comboBox1.SelectedItem);
+            label2.Text = pid;
+            comm.CommandText = "select P_name from Painting where P_id =" + pid;
+            comm.CommandType = CommandType.Text;
+            ds = new DataSet();
+            da = new OracleDataAdapter(comm.CommandText, conn);
+            da.Fill(ds, "Painting");
+            dt = ds.Tables["Painting"];
+            dr = dt.Rows[i];
+            label2.Text = dr["P_name"].ToString();
+            imgchange(label2.Text);
+            likes(pid);
+            conn.Close();
+        }
+        public void likes(string pid)
+        {
+            comm = new OracleCommand();
+            comm.CommandText = "select count(Cust_id) cnt from likes where P_id=" + pid;
+            comm.CommandType = CommandType.Text;
+            ds = new DataSet();
+            da = new OracleDataAdapter(comm.CommandText, conn);
+            da.Fill(ds, "likes");
+            dt = ds.Tables["likes"];
+            dr = dt.Rows[i];
+            label1.Text = dr["cnt"].ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string pid = comboBox1.GetItemText(comboBox1.SelectedItem);
+            db_connect();
+            OracleCommand cum = new OracleCommand();
+            cum.Connection = conn;
+            cum.CommandText = "insert into likes values('" + id + "','" + pid + "')";
+            cum.CommandType = CommandType.Text;
+            cum.ExecuteNonQuery();
+            likes(pid);
+            conn.Close();
+        }
         
     }
 }
